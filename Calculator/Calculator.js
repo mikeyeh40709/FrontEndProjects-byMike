774c9160ecacc87e2288c53801451d54709281ec
MikeYeh <mikeyeh40709@gmail.com>
2020-07-28 14:28:59 +0800

    add pizza edit calculator

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -18,9 +18,15 @@ function insertZero() {
 // 1.開頭是.要放0進去
 // 2.換運算值要改***
 // 3.開根號 / 次方
+// 4.如果輸入的運算值與前一個運算值不相符,換成該運算值
+
 
 function AddCounter(element) {
     if (['+', '-', '*', '/'].includes(document.getElementById('value').value[document.getElementById('value').value.length - 1])) return;
+
+    // if (document.getElementById('value').value[document.getElementById('value').value.length - 1] != `${element.id}`) {
+    //     document.getElementById('value').value.pop().push(`${element.id}`);
+    // }
 
     if (element.id === '-' && document.getElementById('value').value == "") {
         document.getElementById('value').value += `${element.id}`;
@@ -30,7 +36,9 @@ function AddCounter(element) {
 }
 
 function CountResult() {
-    document.getElementById('value').value = eval(document.getElementById('value').value);
+    if (document.getElementById('value').value != '') {
+        document.getElementById('value').value = eval(document.getElementById('value').value);
+    }
 
 }
 
