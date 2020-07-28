31440201c9df76260b1d837033ca3271acad5a60
MikeYeh <mikeyeh40709@gmail.com>
2020-07-28 23:28:52 +0800

    edit calculator, add Apple

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,45 +1,66 @@
 function insertNum(element) {
-    if (element.id === '.' && document.getElementById('value').value.includes('.')) {
-        return;
-    }
     document.getElementById('value').value += element.id;
+    checkZero = true;
+}
+
+//字串後一個是運算值,或input value = "" 後面+=0.
+var checkDot = true;
+
+function insertDot() {
+    if (checkDot == false) {
+        return;
+    } else if (['+', '-', '*', '/'].includes(document.getElementById('value').value[document.getElementById('value').value.length - 1]) || document.getElementById('value').value === '') {
+        document.getElementById('value').value += '0.';
+    } else {
+        document.getElementById('value').value += '.';
+    }
+    checkDot = false;
+    checkZero = true;
 }
 
 function clearAll() {
     document.getElementById('value').value = "";
+    checkDot = true;
+    checkZero = false;
 }
-
+//按下運算值或dot或數字設為true
+var checkZero = false;
 function insertZero() {
-    if (document.getElementById('value').value != "") {
+    if (document.getElementById('value').value != "" && checkZero == true) {
         document.getElementById('value').value += '0';
     }
 }
 
-// 1.開頭是.要放0進去
-// 2.換運算值要改***
 // 3.開根號 / 次方
-// 4.如果輸入的運算值與前一個運算值不相符,換成該運算值
-
+// 5. .1+.3 OK
+// 6. 200 + 0002.3 error
+// 當空的不能+*/
 
 function AddCounter(element) {
-    if (['+', '-', '*', '/'].includes(document.getElementById('value').value[document.getElementById('value').value.length - 1])) return;
-
-    // if (document.getElementById('value').value[document.getElementById('value').value.length - 1] != `${element.id}`) {
-    //     document.getElementById('value').value.pop().push(`${element.id}`);
-    // }
-
-    if (element.id === '-' && document.getElementById('value').value == "") {
+    if (['+', '-', '*', '/', '.'].includes(document.getElementById('value').value[document.getElementById('value').value.length - 1])) {
+        let changeCounter = Array.from(document.getElementById('value').value);
+        changeCounter.pop();
+        changeCounter.push(`${element.id}`);
+        document.getElementById('value').value = changeCounter.join('');
+        
+        // return;
+    } else if (element.id === '-' && document.getElementById('value').value == "") {
         document.getElementById('value').value += `${element.id}`;
     } else if (document.getElementById('value').value != "") {
         document.getElementById('value').value += `${element.id}`;
     }
+    checkDot = true;
+    checkZero = false;
 }
 
 function CountResult() {
-    if (document.getElementById('value').value != '') {
+    if (document.getElementById('value').value != '' && ['+', '-', '*', '/'].includes(document.getElementById('value').value[document.getElementById('value').value.length - 1]) == false) {
         document.getElementById('value').value = eval(document.getElementById('value').value);
+    } else {
+        alert("Invalid Input!");
     }
-
+    checkDot = true;
+    checkZero = false;
 }
 
 function checkForDuplicates(array) {
