dd4b5107c43e728da8db59607b7ff41740d112c2
MikeYeh <mikeyeh40709@gmail.com>
2020-07-30 23:44:22 +0800

    edit MaskMap

--- MaskMap/MaskMap.js
+++ MaskMap/MaskMap.js
@@ -1,24 +1,157 @@
+//用fetch把資料call下來放進button click事件換區域
+//input 改marker
+//scoll後檢查標籤有無改變 消除標籤並重放標籤
 var map;
 var infowindow;
+
 function initMap() {
     map = new google.maps.Map(
         document.getElementById('map'), {
             center: new google.maps.LatLng(25.0412479, 121.5395956),
             zoom: 16,
             mapTypeId: 'roadmap',
-            gestureHandling: 'greedy'
+            styles: [{
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#242f3e'
+                    }]
+                },
+                {
+                    elementType: 'labels.text.stroke',
+                    stylers: [{
+                        color: '#242f3e'
+                    }]
+                },
+                {
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#746855'
+                    }]
+                },
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#263c3f'
+                    }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#6b9a76'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#38414e'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{
+                        color: '#212a37'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#9ca5b3'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#746855'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{
+                        color: '#1f2835'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#f3d19c'
+                    }]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#2f3948'
+                    }]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#17263c'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#515c6d'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{
+                        color: '#17263c'
+                    }]
+                },
+                {
+                    featureType: "poi.business",
+                    stylers: [{
+                        visibility: "off"
+                    }]
+                }
+            ]
         });
     // Create a <script> tag and set the USGS URL as the source.
     map.data.loadGeoJson(
         'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json');
 
-    map.data.setStyle(function(feature){
+    map.data.setStyle(function (feature) {
         var maskLeft = feature.getProperty('mask_adult');
-        if ( maskLeft > 0){
+        if (maskLeft > 0) {
             return {
                 icon: 'https://cdn.frankerfacez.com/emoticon/287489/2'
             };
-        } else{
+        } else {
             return {
                 icon: "https://cdn.frankerfacez.com/emoticon/243754/2"
             };
@@ -38,13 +171,66 @@ function initMap() {
         infowindow.setContent(title + adult + child + updated + address + phone);
         infowindow.setPosition(event.latLng);
         infowindow.setOptions({
-            pixelOffset: new google.maps.Size(0, -34)
+            pixelOffset: new google.maps.Size(0, -55)
         });
         infowindow.open(map);
     });
-}
+};
 
-// function changeArea(place){
+// function initAutocomplete() {
+//     // Create the search box and link it to the UI element.
+//     const input = document.getElementById("pac-input");
+//     const searchBox = new google.maps.places.SearchBox(input);
+//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//     // Bias the SearchBox results towards current map's viewport.
+//     map.addListener("bounds_changed", function() {
+//         searchBox.setBounds(map.getBounds());
+//     });
+//     let markers = [];
+//     // Listen for the event fired when the user selects a prediction and retrieve
+//     // more details for that place.
+//     searchBox.addListener("places_changed", function() {
+//         const places = searchBox.getPlaces();
 
+//         if (places.length == 0) {
+//             return;
+//         }
+//         // Clear out the old markers.
+//         markers.forEach(marker => {
+//             marker.setMap(null);
+//         });
+//         markers = [];
+//         // For each place, get the icon, name and location.
+//         const bounds = new google.maps.LatLngBounds();
+//         places.forEach(place => {
+//             if (!place.geometry) {
+//                 console.log("Returned place contains no geometry");
+//                 return;
+//             }
+//             const icon = {
+//                 url: place.icon,
+//                 size: new google.maps.Size(71, 71),
+//                 origin: new google.maps.Point(0, 0),
+//                 anchor: new google.maps.Point(17, 34),
+//                 scaledSize: new google.maps.Size(25, 25)
+//             };
+//             // Create a marker for each place.
+//             markers.push(
+//                 new google.maps.Marker({
+//                     map,
+//                     icon,
+//                     title: place.name,
+//                     position: place.geometry.location
+//                 })
+//             );
+
+//             if (place.geometry.viewport) {
+//                 // Only geocodes have viewport.
+//                 bounds.union(place.geometry.viewport);
+//             } else {
+//                 bounds.extend(place.geometry.location);
+//             }
+//         });
+//         map.fitBounds(bounds);
+//     });
 // }
-
